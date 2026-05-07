414950a189a2ef169c53fdd406b74674dcec92e9
vnntonnio <acevalantonio@gmail.com>
2026-05-06 21:06:33 -0400

    fix: add CORS configuration to allow frontend requests

--- src/main/java/com/citt/config/CorsConfig.java
+++ src/main/java/com/citt/config/CorsConfig.java
@@ -7,18 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
-        public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Aplica a todos los endpoints
-                        .allowedOrigins("*") // Permite cualquier origen
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                        .allowedHeaders("*") // Permite cualquier cabecera
-                        .allowCredentials(false); // Deshabilita credenciales compartidas (true si necesitas cookies o autenticación)
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
-
 }
